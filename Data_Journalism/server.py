1659d51f3806c239ec41d82e8460863ab70708c9
SaavanG <saavan.gopal26@trinityschoolnyc.org>
2025-06-03 16:38:09 -0400

    fix/upload prototypes

--- Data_Journalism/server.py
+++ Data_Journalism/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, send_from_directory
 import json
-from static import images
+
 
 
 app = Flask(__name__, static_url_path='', static_folder='static')
