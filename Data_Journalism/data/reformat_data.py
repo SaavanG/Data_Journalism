42360b16b9b730e6cebdf4c4dd07d6fc44e3e6f7
SaavanG <saavan.gopal26@trinityschoolnyc.org>
2025-06-04 21:17:08 -0400

    file names

--- Data_Journalism/data/reformat_data.py
+++ Data_Journalism/data/reformat_data.py
@@ -1,19 +1,33 @@
 import json
 
-file = open("data/data_cleansed.csv", "r")
+file = open("Data_Journalism/data/data_cleansed.csv", 'r') 
 lines = file.readlines()
 
-borough_acreage = {}
+borough_zip_acreage = {}
 
 for i in range(1, len(lines)):
-    line = lines[i]
-    data = line.strip().split(',')
-    
-    acreage = float(data[0])
-    borough = data[2]
-    
-    if borough not in borough_acreage:
-        borough_acreage[borough] = 0
-    borough_acreage[borough] += acreage
+   line = lines[i]
+   data = line.strip().split(',')
+
+   acreage = float(data[0])
+   borough = data[2]
+   zipcode = data[3].strip(',')
+
+   zipcode = zipcode.replace('"', '').strip()
+
+   if ',' in zipcode:
+       zipcodes = [z.strip() for z in zipcode.split(',')]
+   else:
+       zipcodes = [zipcode]
+   if borough not in borough_zip_acreage:
+       borough_zip_acreage[borough] = {}
+
+   for zipcode in zipcodes:
+       if zipcode not in borough_zip_acreage[borough]:
+          borough_zip_acreage[borough][zipcode] = 0
+       borough_zip_acreage[borough][zipcode] += acreage
 
 file.close()
+
+with open("Data_Journalism/data/data.json", 'w') as json_file:
+    json.dump(borough_zip_acreage, json_file, indent= 3)
